7b94cd504aef986718c0d648a299b6ad5f31a38e
Constant Likudie <likudie@gmail.com>
2023-05-16 23:37:30 -0700

    init extension

--- chrome-extension/dadjokes.js
+++ chrome-extension/dadjokes.js
@@ -1,1 +1,8 @@
-alert('hello');
+const jokeElement = document.getElementById("joke");
+
+fetch('https://loljokes.azurewebsites.net/jokes').then(r => r.text()).then(result => {
+    jokeElement.innerHTML = result;
+}).catch(err => {
+    // add some default joke here
+    jokeElement.innerHTML = JSON.stringify(err);
+});
