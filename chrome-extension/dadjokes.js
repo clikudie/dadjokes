646ef20b853af0c7d2a0b87e2e994dddccad2f6f
Constant Likudie <likudie@gmail.com>
2023-05-17 22:44:11 -0700

    add styles to extension

--- chrome-extension/dadjokes.js
+++ chrome-extension/dadjokes.js
@@ -1,8 +1,30 @@
 const jokeElement = document.getElementById("joke");
+const nextButton = document.getElementById("nextBtn");
+let cachedJoke = '';
+const defaultJoke = 'I\'m afraid for the calendar. Its days are numbered.'
 
-fetch('https://loljokes.azurewebsites.net/jokes').then(r => r.text()).then(result => {
-    jokeElement.innerHTML = result;
-}).catch(err => {
-    // add some default joke here
-    jokeElement.innerHTML = JSON.stringify(err);
+async function callJokesAPI() {
+    const r = await fetch('https://loljokes.azurewebsites.net/jokes');
+    return await r.json();
+}
+
+async function displayJoke() {
+    try {
+        cachedJoke = await callJokesAPI();
+        jokeElement.innerHTML = cachedJoke;
+    } catch (err) {
+        console.log(JSON.stringify(err));
+        jokeElement.innerHTML = cachedJoke ? cachedJoke : defaultJoke;
+    }
+}
+
+async function startApp() {
+    jokeElement.innerHTML = defaultJoke;
+    await displayJoke();
+}
+
+nextButton.addEventListener('click', async () => {
+    await displayJoke();
 });
+
+startApp();
