02bd2169b695209a9ea3c3a15739bfd0f106d089
Constant Likudie <likudie@gmail.com>
2023-05-27 16:31:14 -0700

    fix broken .env file not found

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -1,13 +1,7 @@
 import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-console.log(process.env.NODE_ENV);
-
-const envFound = dotenv.config();
-if (envFound.error) {
-    console.log(envFound.error);
-    throw new Error('.env file not found');
-}
+dotenv.config();
 
 export default {
     port: process.env.PORT || 10
