a4fde5aad612416e1f4e8f59caf5c43777d36b66
Constant Likudie <likudie@gmail.com>
2023-05-27 16:20:43 -0700

    see dotenv error when deployed

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log(process.env.NODE_ENV);
 
 const envFound = dotenv.config();
 if (envFound.error) {
+    console.log(envFound.error);
     throw new Error('.env file not found');
 }
 
