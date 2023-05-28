44256dc294d0fe240216ffbdaa6770de8ef08422
Constant Likudie <likudie@gmail.com>
2023-05-27 20:19:40 -0700

    debugging

--- api/src/loaders/index.js
+++ api/src/loaders/index.js
@@ -2,6 +2,7 @@ import { expressLoader } from "./express.js";
 import { connectDb } from "./mongoose.js";
 
 export async function load(app) {
+    console.log('Process env: ', process.env)
     await connectDb();
     expressLoader(app);
 }
