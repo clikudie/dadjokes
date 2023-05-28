2f3cf081ef134ce11f2cb6333eaafed06a3f17fc
Constant Likudie <likudie@gmail.com>
2023-05-27 20:41:58 -0700

    update extension code

--- api/src/loaders/index.js
+++ api/src/loaders/index.js
@@ -2,7 +2,6 @@ import { expressLoader } from "./express.js";
 import { connectDb } from "./mongoose.js";
 
 export async function load(app) {
-    console.log('Process env: ', process.env)
     await connectDb();
     expressLoader(app);
 }
